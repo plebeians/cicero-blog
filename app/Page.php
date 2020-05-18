885f62251543fe5356fe434bdf15ea86bee5a377
Maslov Timur <dev@maslov24.ru>
2020-05-18 20:23:57 +0500

    Создал модель Page

---
+++ app/Page.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Page extends Model
+{
+
+    use SoftDeletes;
+
+    protected $fillable = [
+        'description',
+        'text',
+        'slug',
+        'sort',
+        'seo_description',
+        'seo_keywords',
+        'title',
+    ];
+}
