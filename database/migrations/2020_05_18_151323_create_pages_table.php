ea08b1a4f46f623e5baee89beea9ba74a6d77389
Maslov Timur <dev@maslov24.ru>
2020-05-18 20:23:40 +0500

    Создал миграцию CreatePagesTable

---
+++ database/migrations/2020_05_18_151323_create_pages_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePagesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('pages', function (Blueprint $table) {
+            $table->id();
+            $table->text('description');
+            $table->text('text');
+            $table->string('slug');
+            $table->unsignedBigInteger('sort');
+            $table->softDeletes();
+            $table->string('seo_description');
+            $table->string('seo_keywords');
+            $table->string('title');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('pages');
+    }
+}
